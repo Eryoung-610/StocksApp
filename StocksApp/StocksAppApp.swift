34bc1c6cd2a566bd5eb5a1a4662dfb205167a71e
Eric <eryoung610@gmail.com>
2023-07-10 15:17:03 -0700

    Initial Commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Eric Young on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
